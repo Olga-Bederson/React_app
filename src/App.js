8e23bce57bc8655a21691abef58bc4bde88b5beb
Olga-Bederson <osbederson@gmail.com>
2022-02-21 22:29:59 +0500

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import './App.css';
 import {Message} from "./components/Message/index_message";
+import React from 'react';
+import {useEffect, useState} from "react";
 
-const appText = "Hello!";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [messageText, setMessageText] = useState('');
+
+  const sendMessage = (newMessage) => {
+    setMessages([...messages,  newMessage]);
+  }
+
+  const submitMessage = () => {
+    if (messageText === '') {
+      return;
+    }
+    sendMessage({
+      author: "me",
+      text: messageText,
+    });
+    setMessageText("");
+  }
+
+  useEffect(() => {
+    if (messages.length === 0) {
+      return;
+    }
+    const tail = messages[messages.length - 1];
+    if (tail.author === "bot") {
+      return;
+    }
+    setTimeout(() => {
+      sendMessage({author:"bot", text: "I`m bot"});
+    },  1500);
+  });
+
   return (
     <div className="App">
-      <Message text={appText}/>
+      <div>
+        <input
+          value={messageText}
+          onChange={(event) => {
+            setMessageText(event.target.value);
+          }}
+        />
+        <button onClick={submitMessage}>Send</button>
+      </div>
+      <div className="messages">
+        {messages.map((message, i) => <Message key={i} text={message.text}/>)}
+      </div>
     </div>
   );
 }
